00a47b6a993a2d1f057e4910eba99498ed33dd6b
chicheng Feng <gazwo@hotmail.com>
2017-06-16 21:13:16 +1200

    update

--- js/jquery.shop.js
+++ js/jquery.shop.js
@@ -29,9 +29,9 @@
 			this.$paypalForm = this.$element.find( "#paypal-form" ); // PayPal form
 			
 			
-			this.currency = ""; // HTML entity of the currency to be displayed in the layout
-			this.currencyString = "＄"; // Currency symbol as textual string
-			this.paypalCurrency = "NZD"; // PayPal's currency code
+			this.currency = "&euro;"; // HTML entity of the currency to be displayed in the layout
+			this.currencyString = "€"; // Currency symbol as textual string
+			this.paypalCurrency = "EUR"; // PayPal's currency code
 			this.paypalBusinessEmail = "yourbusiness@email.com"; // Your Business PayPal's account email address
 			this.paypalURL = "https://www.sandbox.paypal.com/cgi-bin/webscr"; // The URL of the PayPal's form
 			
